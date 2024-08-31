2f11da6a19a507c3c170911a1cade9ead470c4a6
kshah5225 <kshah5225@gmail.com>
2024-08-30 22:50:31 -0700

    hopperv4 test working

--- Kunal's Testing grounds/test.py
+++ Kunal's Testing grounds/test.py
@@ -1,14 +1,18 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 # Create the Hopper environment
 env = gym.make('Hopper-v4', render_mode='human')
+
+# Wrap the environment with DummyVecEnv
+env = DummyVecEnv([lambda: env])
 
 # Initialize the PPO model
 model = PPO('MlpPolicy', env, verbose=1)
 
 # Train the model
-model.learn(total_timesteps=100000)
+model.learn(total_timesteps=1000000)
 
 # Save the trained model
 model.save("ppo_hopper")
