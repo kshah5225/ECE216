05fcac052760847d800441021ce60fc3df674a10
Maximillian Sayre <100393921+Ziblo@users.noreply.github.com>
2024-08-31 12:37:56 -0700

    added instructions to kangaroo_registration

--- kangaroo_registration.py
+++ kangaroo_registration.py
@@ -1,7 +1,14 @@
-from gymnasium.envs.registration import register
+'''
+Add this to line 185 of this (vvvv) file
+C:\Users\\AppData\Local\Programs\Python\Python312\Lib\site-packages\gymnasium\envs\__init__.py
 
 register(
     id="gym_examples/Kangaroo-v0",
     entry_point="gym_examples.envs:kangaroo_env",
     max_episode_steps=300,
 )
+
+Also add this to line 16 of this (vvvv) file
+C:\Users\Maximillian Sayre\AppData\Local\Programs\Python\Python312\Lib\site-packages\gymnasium\envs\mujoco\__init__.py
+from gymnasium.envs.mujoco.kangaroo_env import KangarooEnv
+'''
