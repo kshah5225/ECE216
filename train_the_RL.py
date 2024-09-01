c63eb203a925d7627e7ae8cc4dbb3b8b437fea5f
kshah5225 <kshah5225@gmail.com>
2024-09-01 13:28:28 -0700

    3 Jumps

--- train_the_RL.py
+++ train_the_RL.py
@@ -1,11 +1,10 @@
 import gymnasium as gym
-from stable_baselines3 import PPO
+from stable_baselines3 import A2C
 from stable_baselines3.common.vec_env import DummyVecEnv
 
 env = gym.make('Kangaroo-v0', render_mode='human')
 env = DummyVecEnv([lambda: env])
-model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="logs")
-model.learn(total_timesteps=1000000)
-model.save("ppo_kangaroo")
-
+model = A2C('MlpPolicy', env, verbose=1, tensorboard_log="logs")
+model.learn(total_timesteps=250000*1000)
+model.save("A2C_kangaroo")
 env.close()
